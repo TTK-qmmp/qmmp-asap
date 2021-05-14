5510ddee0b2dd618290105d82cbbc76c85c45ca6
greedysky <greedysky@163.com>
2021-05-14 21:29:42 +0800

    refactor: Optimized input module

--- asaphelper.h
+++ asaphelper.h
@@ -28,12 +28,10 @@ extern "C" {
 
 typedef struct {
     ASAP *asap;
-    int sample_rate;
     int length;
     int bitrate;
-    int bits_per_sample;
     int channels;
-} asap_info_t;
+} asap_info;
 
 /*!
  * @author Greedysky <greedysky@163.com>
@@ -64,7 +62,7 @@ public:
 
 private:
     QString m_path;
-    asap_info_t *m_info;
+    asap_info *m_info;
     QMap<QString, QString> m_meta;
 
 };
