951d403e3158269f8958b6fdcf8d0131cb169feb
Greedysky <greedysky@163.com>
2026-03-20 15:10:30 +0800

    feat: Support for qmmp 2.4

--- decoderasapfactory.cpp
+++ decoderasapfactory.cpp
@@ -40,19 +40,25 @@ Decoder *DecoderAsapFactory::create(const QString &path, QIODevice *input)
     return new DecoderAsap(path);
 }
 
-QList<TrackInfo*> DecoderAsapFactory::createPlayList(const QString &path, TrackInfo::Parts parts, QStringList *)
+TrackInfoList DecoderAsapFactory::createPlayList(const QString &path, TrackInfo::Parts parts, QStringList *)
 {
-    TrackInfo *info = new TrackInfo(path);
+#if QMMP_VERSION_INT < 0x20400
+    TrackInfo *raw(new TrackInfo(path)), *info = raw;
+#else
+    TrackInfo raw(path), info = &raw;
+#endif
     if(parts == TrackInfo::Parts())
     {
-        return QList<TrackInfo*>() << info;
+        return {raw};
     }
 
     AsapHelper helper(path);
     if(!helper.initialize())
     {
+#if QMMP_VERSION_INT < 0x20400
         delete info;
-        return QList<TrackInfo*>();
+#endif
+        return {};
     }
 
     if(parts & TrackInfo::MetaData)
@@ -71,7 +77,8 @@ QList<TrackInfo*> DecoderAsapFactory::createPlayList(const QString &path, TrackI
         info->setValue(Qmmp::FORMAT_NAME, "Another Slight Atari Player");
         info->setDuration(helper.totalTime());
     }
-    return QList<TrackInfo*>() << info;
+
+    return {raw};
 }
 
 MetaDataModel* DecoderAsapFactory::createMetaDataModel(const QString &path, bool readOnly)
